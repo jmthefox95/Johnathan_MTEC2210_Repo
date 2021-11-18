9ed370313d6b409c152189b9441ba23c75aff48f
Johnathan Mahones <jmthefox@live.com>
2021-11-18 15:40:42 -0500

    Update again

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
+    public float SpeedMul = 1f;
+    private Rigidbody2D rb;
 
     public GameObject playerPig;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<RigidBody2D>();
 
     }
 
