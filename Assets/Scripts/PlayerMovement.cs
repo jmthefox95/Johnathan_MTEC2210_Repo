2639fe992adaf22ffd11c8aa490dd0c5ad6ae78e
Johnathan Mahones <jmthefox@live.com>
2021-11-18 18:15:41 -0500

    Moved Glossary script outside project folder due to compiler errors

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,13 +11,15 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<RigidBody2D>();
+        rb = GetComponent<Rigidbody2D>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        float xPos = Input.GetAxis("Horizonatl");
+        float speed = Time.deltaTime * speedMul * xPos;
 
         if (Input.GetKey(KeyCode.D))
         {
