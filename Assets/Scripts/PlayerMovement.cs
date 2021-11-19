e7890d032b6e9bc5930585b05e64fc6febaf7073
Johnathan Mahones <jmthefox@live.com>
2021-11-18 20:32:36 -0500

    PlayerMovement and PlayerController finally works.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,24 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class playerMovement : MonoBehaviour
 {
-    public float SpeedMul = 1f;
+
+    public float speed;
+    public float jumpingSpeed = 1f;
+
+    private SpriteRenderer sr;
     private Rigidbody2D rb;
+
 
     public GameObject playerPig;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xPos = Input.GetAxis("Horizonatl");
-        float speed = Time.deltaTime * speedMul * xPos;
 
         if (Input.GetKey(KeyCode.D))
         {
